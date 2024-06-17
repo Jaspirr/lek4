9e17b539dba1ee058704e3479dfa115933677891
Erlandsson <jesper.erlandsson@cgi.com>
2024-06-17 09:52:42 +0200

    added lock in

--- lek4/Components/Service/NumberService.cs
+++ lek4/Components/Service/NumberService.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace lek4.Components.Service
 {
     public class NumberService
     {
         public double CurrentNumber { get; set; }
+
+        public List<int> UnlockedProducts { get; private set; } = new List<int>();
+        public List<int> LockedInProducts { get; private set; } = new List<int>();
+
+        public void UnlockProduct(int productNumber)
+        {
+            if (!UnlockedProducts.Contains(productNumber))
+            {
+                UnlockedProducts.Add(productNumber);
+            }
+        }
+
+        public void LockInProduct(int productNumber)
+        {
+            if (!LockedInProducts.Contains(productNumber))
+            {
+                LockedInProducts.Add(productNumber);
+            }
+        }
+
+        public bool IsProductUnlocked(int productNumber)
+        {
+            return UnlockedProducts.Contains(productNumber);
+        }
+
+        public bool IsProductLockedIn(int productNumber)
+        {
+            return LockedInProducts.Contains(productNumber);
+        }
     }
 }
